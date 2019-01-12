2f04493c25900f1be63aa51b9ce70fc5f889b03c
Roberto Vite <roberto.vite@hotmail.com>
2019-01-12 12:43:51 -0600

    Terminando te probar componentes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, AlertController, LoadingController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,8 +8,11 @@ import { Router } from '@angular/router';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  
 
-  constructor(public hoja: ActionSheetController, public camino: Router) { }
+  constructor(public hoja: ActionSheetController, public camino: Router, public alerta: AlertController, public cargando: LoadingController) {
+
+   }
 
   async llamarHoja1(){
     const hoja1 = await this.hoja.create({
@@ -28,6 +31,26 @@ export class Tab2Page {
     }]
     });
     await hoja1.present();
+  }
+
+  async llamarAlerta1(){
+    const alerta1 = await this.alerta.create({
+      header: "Alerta ¡AMBER!",
+      subHeader: "No seas Necio...",
+      message: "Escribe tu nombre y luego largate a la pantalla anterior",
+      inputs: [{
+        name: "nombre",
+        type: "text",
+        placeholder: "Tu Nombre y luego vete"
+      }],
+      buttons: [{
+        text: "Ok",
+        handler: () => {
+          this.regresar();
+        }
+      }]
+    });
+    await alerta1.present();
   }
 
   regresar(){
